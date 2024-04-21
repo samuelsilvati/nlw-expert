4306d2b3d8c2236bc7ac021116e85d19cec28f80
Samuel Silva <samuel7silva@outlook.com>
2024-04-21 11:13:08 -0300

    fix: header date

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -15,7 +15,10 @@ export default function NoteCard({ note }: NoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className="relative flex flex-col gap-3 overflow-hidden rounded-md bg-slate-800 p-5 text-left outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-200">
-          {note.date.toISOString()}
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className="text-sm leading-6 text-slate-400">{note.content}</p>
         <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
